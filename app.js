4822b2a7dff7deec73aadce9fdb33d3f70a21473
josephh27 <josephjasonsbuhain@iskolarngbayan.pup.edu.ph>
2023-03-11 22:38:21 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+//Toggling between dark and light mode
+document.querySelector('.dark-mode-toggle').addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    const isDarkMode = document.body.classList.contains('dark');
+    localStorage.setItem('darkmode', isDarkMode);
+})
+
+//Initial values
+//Screens
+const startScreen = document.querySelector(".start-screen");
+const inGameScreen = document.querySelector(".in-game-screen");
+const pauseScreen = document.querySelector(".pause-screen");
+const resultScreen = document.querySelector(".result-screen");
+
+//Others
+const nameInput = document.querySelector("#name-input");
+const cells = document.querySelectorAll(".grid-cell");
+
+//UI in game at the bottom
+const numbersInput = document.querySelectorAll(".numbers");
+const nameDisplay = document.querySelector("#in-game-name");
+const levelDisplay = document.querySelector("#in-game-level");
+const timeDisplay = document.querySelector("#game-time");
+
+let levelIndex = 0;
+let level = CONSTANT.MODES[levelIndex];
+
+let timer = null;
+let pause = false;
+let seconds = 0;
+
+let su = undefined;
+let su_answer = undefined;
+
+let
+
+
---
+++ information.js
@@ -0,0 +1,15 @@
+const CONSTANT = {
+    UNASSIGNED: 0,
+    GRID_SIZE: 9,
+    BOX_SIZE: 3,
+    NUMBERS: [1,2,3,4,5,6,7,8,9],
+    MODE_NAMES: [
+        'Easy',
+        'Medium',
+        'Hard',
+        'Very Hard',
+        'Insane',
+        'Inhuman'
+    ],
+    MODES: [29, 38, 47, 56, 65, 74]
+}
